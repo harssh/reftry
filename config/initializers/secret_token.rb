442e810e9a0495af84d90a225d3081a6667429c0
Harssh <harssh@headfitted.com>
2013-01-28 14:27:10 +0530

    working on css

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reftry::Application.config.secret_token = 'f74a7eb441702f61da66a889d3121dbea0f671e1d7b838dd49e12778f54eee747dc9e8fa866ef5ef092a0f9f0d71edc427919de6e101221c48a436a4380e98a2'
